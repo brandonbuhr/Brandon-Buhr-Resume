e4c3dc8eba66b57d69fd2c0028661ff865e21fc8
brandonbuhr <brandonbuhr35@gmail.com>
2024-07-03 01:20:17 -0500

    simplified layout structure

--- src/app/page.js
+++ src/app/page.js
@@ -5,59 +5,25 @@ function MainTitle() {
     <>
     <div className="main-title">
       <h4>Brandon Buhr</h4>
-      <h4>Aspiring Software Developer</h4>
+      <h4>Software Developer</h4>
       </div>
       </>
   );
 };
-
-function Education() {
-  return (
-    <>
-    <div className="education">
-    <section>
-       <h1>Projects</h1>
-       <h2><a href="https://react-weather-app-murex-phi.vercel.app/" target="blank" className="weather-app">React Weather App</a></h2>
-          <p>A weather app deployed using Vercel. This project is built with React, utilizing a weather API to fetch data, and all the data handling is done with JavaScript and JSX. The app's styling is crafted with CSS.</p>
-          <h2><a href="https://tic-tac-toe-pi-ashen.vercel.app/" target="blank" className="tic-tac-toe-project">Tic-Tac-Toe Project</a></h2>
-            <p>A simple tic-tac-toe game made using React.</p>
-            <h2><a href="https://github.com/brandonbuhr" target="blank" className="my-github">My Github</a></h2>
-            
-      </section>
-      <br />
-    <section>
-       <h1>Education</h1>
-          <h2>Bachelor of Fine Arts in Music Composition and Technology</h2>
-            <h3>University of Wisconsin-Milwaukee, Peck School of the Arts, 2016-2021</h3>
-            <li>Experience with multiple DAWs and other software including: Ableton, Fl Studio, Studio One, Digital Performer, and Vocaloid.</li>
-            <li>Collaborated with artists in other fields such as film and dance, designing sound and producing music</li>
-            <li>Experience with mixing, mastering, editing, and producing audio</li>
-            <li>I operate my own Youtube channel and regularly post my music on various online platforms here: <a 
-              className="music-portfolio" 
-              href="https://www.hyperfollow.com/brandonbuhr" 
-              target="blank">Music Portfolio
-              </a>
-            </li>
-      </section>
-      <br />
-      <section>
-          <h2>Software Development</h2>
-            <h3>Course Careers, 2024</h3>
-            <li>This resume was made using React</li>
-            <li>Experience working with Go, JavaScript, React, HTML, CSS, APIs</li>
-            <li>Knowledge of data structures and algorithms, as well as time complexity</li>
-      </section>
-    </div>
-    </>
-  );
-}
 
 function WorkExperience() {
   return (
     <>
     <div className="work-experience">
       <section>
-       <h1>Work Experience</h1>
+       <h1>Experience</h1>
+       
+       <h2><a href="https://react-weather-app-murex-phi.vercel.app/" target="blank" className="weather-app">React Weather App</a></h2>
+          <p>A weather app deployed using Vercel. This project is built with React, utilizing a weather API to fetch data, and all the data handling is done with JavaScript and JSX. The app's styling is crafted with CSS.</p>
+          <h2><a href="https://tic-tac-toe-pi-ashen.vercel.app/" target="blank" className="tic-tac-toe-project">Tic-Tac-Toe Project</a></h2>
+            <p>A simple tic-tac-toe game made using React.</p>
+            <h2><a href="https://github.com/brandonbuhr" target="blank" className="my-github">Github Projects</a></h2>
+        <br />
           <h2>Sales Representative</h2>
             <h3>Cellular Sales, March 2022 - May 2024</h3>
              <li>Engaged with clients in a retail setting to understand their needs and recommend suitable products.</li>
@@ -88,12 +54,43 @@ function WorkExperience() {
   );
 }
 
+function Education() {
+  return (
+    <>
+    <div className="education">
+    <section>
+       <h1>Education</h1>
+          <h2>Bachelor of Fine Arts in Music Composition and Technology</h2>
+            <h3>University of Wisconsin-Milwaukee, Peck School of the Arts, 2016-2021</h3>
+            <li>Experience with multiple DAWs and other software including: Ableton, Fl Studio, Studio One, Digital Performer, and Vocaloid.</li>
+            <li>Collaborated with artists in other fields such as film and dance, designing sound and producing music</li>
+            <li>Experience with mixing, mastering, editing, and producing audio</li>
+            <li>I operate my own Youtube channel and regularly post my music on various online platforms here: <a 
+              className="music-portfolio" 
+              href="https://www.hyperfollow.com/brandonbuhr" 
+              target="blank">Music Portfolio
+              </a>
+            </li>
+      </section>
+      <br />
+      <section>
+          <h2>Software Development</h2>
+            <h3>Course Careers, 2024</h3>
+            <li>This resume was made using React</li>
+            <li>Experience working with Go, JavaScript, React, HTML, CSS, APIs</li>
+      </section>
+    </div>
+    </>
+  );
+}
+
 export default function Portfolio() {
   return ( 
   <div>
     <MainTitle />
-      <Education />
       <WorkExperience />
+      <Education />
   </div>
+
   );
 }
